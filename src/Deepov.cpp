14c349eda1ccdf5d8d1d0cd28115115250386c86
Romain <romain.goussault@gmail.com>
2014-08-30 00:06:44 +1000

    hello world test

---
+++ src/Deepov.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Deepov.cpp
+// Author      : Romain Goussault
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
