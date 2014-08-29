2c40b5d911c40b1f7d14ac4574c000b00096abe9
Romain <romain.goussault@gmail.com>
2014-08-30 00:24:56 +1000

    test jenkins hook

--- src/Deepov.cpp
+++ src/Deepov.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World Deepov!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
