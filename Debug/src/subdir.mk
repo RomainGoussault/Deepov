61169ec88a9abe30ac59fac8f5136c9581e30a2a
Romain <romain.goussault@gmail.com>
2014-08-30 23:28:10 +1000

    move back position.hpp  to src folder

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"C:\Users\Romain\git\c++\Deepov\include" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
