93fba3916144aa6fd80a4ec85de4ab3344d50629
Romain Goussault <romain.goussault@gmail.com>
2014-10-27 22:15:14 +1100

    fix knight test

--- test/KnightTest.cpp
+++ test/KnightTest.cpp
@@ -6,16 +6,14 @@
 
 TEST_CASE( "Knight possible moves ", "[Knight]" )
 {
-	SECTION("Knight blocked by ennemy pieces")
+	SECTION("Knight free")
 	{
-		Board board("8/2r1r3/8/3K4/5b2/8/8/8 w - - 0 1");
-		Position position(4,4);
+		Board board("8/4k3/8/3K4/4N3/8/8/8 w - - 0 1");
+		Position position(4,3);
 		PiecePtr KnightPtr = board.getPiecePtr(position);
 
 	    int size = KnightPtr->getPseudoLegalMoves(board).size();
-	    REQUIRE(size == 5);
+	    REQUIRE(size == 8);
 	}
-
-
 }
 
