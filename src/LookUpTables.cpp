e7ccb63def418f8e0ce463bdb3a4f38510d2b258
Navid <navid.hedjazian@gmail.com>
2015-04-26 16:24:48 +0200

    Implement LookUpTables

--- src/LookUpTables.cpp
+++ src/LookUpTables.cpp
@@ -1,5 +1,42 @@
 #include "LookUpTables.hpp"
 
-   //LookUpTables::MaskRank[1]=0x00000000000000FF;
-  // LookUpTables::test = 0;
+    static const U64 MASK_RANK[]={0xFF,
+                                0xFF00,
+                                0xFF0000,
+                                0xFF000000,
+                                0xFF00000000,
+                                0xFF0000000000,
+                                0xFF000000000000,
+                                0xFF00000000000000
+                                };
+
+    static const U64 MASK_FILE[]={0x0101010101010101,
+                                0x202020202020202,
+                                0x404040404040404,
+                                0x808080808080808,
+                                0x1010101010101010,
+                                0x2020202020202020,
+                                0x4040404040404040,
+                                0x8080808080808080
+                                };
+
+    static const U64 CLEAR_RANK[]={0xFFFFFFFFFFFFFF00,
+                                0xFFFFFFFFFFFF00FF,
+                                0xFFFFFFFFFF00FFFF,
+                                0xFFFFFFFF00FFFFFF,
+                                0xFFFFFF00FFFFFFFF,
+                                0xFFFF00FFFFFFFFFF,
+                                0xFF00FFFFFFFFFFFF,
+                                0x00FFFFFFFFFFFFFF
+                                };
+
+    static const U64 CLEAR_FILE[]={0xFEFEFEFEFEFEFEFE,
+                                0xFDFDFDFDFDFDFDFD,
+                                0xFBFBFBFBFBFBFBFB,
+                                0xF7F7F7F7F7F7F7F7,
+                                0xEFEFEFEFEFEFEFEF,
+                                0xDFDFDFDFDFDFDFDF,
+                                0xBFBFBFBFBFBFBFBF,
+                                0x7F7F7F7F7F7F7F7F
+                                };
 
