1b7941ecce3f75cfd27129736c7d15439fd5d98d
Romain Goussault <romain.goussault@gmail.com>
2015-05-01 23:02:33 +0200

    add toString method for Move

--- src/FastMove.hpp
+++ src/FastMove.hpp
@@ -1,14 +1,18 @@
 #ifndef FASTMOVE_H
 #define FASTMOVE_H
 
+#include <iostream>
+
 class FastMove
 {
 
 public:
+
 	FastMove(unsigned int origin, unsigned int destination, unsigned int flags)
 {
 		myMove = ((flags & 0xf)<<12) | ((origin & 0x3f)<<6) | (destination & 0x3f);
 }
+
 	unsigned int getDestination() const {
 		return myMove & 0x3f;
 	}
@@ -23,5 +27,15 @@ private:
 	int myMove; //Bits : Flags 4 bits ||  Origin 6 bits ||  Destination (6 bits)
 
 };
+
+
+inline std::ostream& operator<<(std::ostream &strm, const FastMove &fastmove) {
+
+	strm << "Origin: " << fastmove.getOrigin() << " Dest: "
+			<< fastmove.getDestination() << " Flags: " << fastmove.getFlags()
+			<< std::endl;
+
+	return strm;
+}
 
 #endif // FASTMOVE_H
