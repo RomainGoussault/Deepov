845a5dd1f420addfa6de0a2c3203174707afa5ff
Romain Goussault <romain.goussault@gmail.com>
2015-05-03 15:45:40 +0200

    Update Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,15 +9,21 @@
 #include <iostream>
 #include <vector>
 
-#include "Position.hpp"
 #include "Color.hpp"
-#include "Board.hpp"
-#include "Rook.hpp"
 #include "FastBoard.hpp"
 
 int main() {
-	FastBoard fb = FastBoard();
+	FastBoard fb = FastBoard("8/1b2kB2/7b/8/8/2qK4/8/8 w - -");
+
+	std::cout << FastBoard::printBitBoard(fb.getBlackBishops()) << std::endl;
 	std::cout << fb << std::endl;
+
+	std::vector<FastMove> v = fb.getKingPseudoLegalMoves(WHITE) ;
+
+	for(std::vector<FastMove>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
 
 	return 0;
 }
