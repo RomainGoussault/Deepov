d464b5e7fd0d1994497a0c48833cc1a005270f03
Romain Goussault <romain.goussault@gmail.com>
2015-05-03 15:33:18 +0200

    Add more knight tests

--- test/KnightMovesTest.cpp
+++ test/KnightMovesTest.cpp
@@ -3,12 +3,21 @@
 
 TEST_CASE( "Knight moves", "[knight]" )
 {
-	SECTION("Test pseudo-legal moves")
+	SECTION("Test pseudo-legal moves with one knight")
 	{
 		FastBoard board("8/4kB2/8/5b2/8/1bqKN3/8/n7 w - -");
 		int whiteSize = board.getKnightPseudoLegalMoves(WHITE).size();
 		int blackSize = board.getKnightPseudoLegalMoves(BLACK).size();
 		REQUIRE(whiteSize == 8);
 		REQUIRE(blackSize == 1);
+	}
+
+	SECTION("Test pseudo-legal moves with 2 knights")
+	{
+		FastBoard board("8/1N6/3q4/8/3r4/3B4/8/7N w - -");
+		int whiteSize = board.getKnightPseudoLegalMoves(WHITE).size();
+		int blackSize = board.getKnightPseudoLegalMoves(BLACK).size();
+		REQUIRE(whiteSize == 6);
+		REQUIRE(blackSize == 0);
 	}
 }
