b8c286f4387f47687de4c0a9102b4529b4c8828b
Romain Goussault <romain.goussault@gmail.com>
2015-06-09 22:47:36 +0200

    Refactor LookUpTables, using namespace

--- src/LookUpTables.hpp
+++ src/LookUpTables.hpp
@@ -1,25 +1,50 @@
 #ifndef LOOKUPTABLES_HPP_
 #define LOOKUPTABLES_HPP_
 
-#include <cstdint>
-#include <iostream>
-#include <bitset>
-#include "FastBoard.hpp"
-
-
-class LookUpTables
+namespace LookUpTables
 {
-public:
-    static const U64 CLEAR_RANK[8];
-    static const U64 MASK_RANK[8];
-    static const U64 CLEAR_FILE[8];
-    static const U64 MASK_FILE[8];
+    const U64 MASK_RANK[]={0xFF,
+                                0xFF00,
+                                0xFF0000,
+                                0xFF000000,
+                                0xFF00000000,
+                                0xFF0000000000,
+                                0xFF000000000000,
+                                0xFF00000000000000
+                                };
 
-    static const U64 ROOK_INITIAL_POS;
+    const U64 MASK_FILE[]={0x0101010101010101,
+                                0x202020202020202,
+                                0x404040404040404,
+                                0x808080808080808,
+                                0x1010101010101010,
+                                0x2020202020202020,
+                                0x4040404040404040,
+                                0x8080808080808080
+                                };
 
-private:
+    const U64 CLEAR_RANK[]={0xFFFFFFFFFFFFFF00,
+                                0xFFFFFFFFFFFF00FF,
+                                0xFFFFFFFFFF00FFFF,
+                                0xFFFFFFFF00FFFFFF,
+                                0xFFFFFF00FFFFFFFF,
+                                0xFFFF00FFFFFFFFFF,
+                                0xFF00FFFFFFFFFFFF,
+                                0x00FFFFFFFFFFFFFF
+                                };
 
-};
+    const U64 CLEAR_FILE[]={0xFEFEFEFEFEFEFEFE,
+                                0xFDFDFDFDFDFDFDFD,
+                                0xFBFBFBFBFBFBFBFB,
+                                0xF7F7F7F7F7F7F7F7,
+                                0xEFEFEFEFEFEFEFEF,
+                                0xDFDFDFDFDFDFDFDF,
+                                0xBFBFBFBFBFBFBFBF,
+                                0x7F7F7F7F7F7F7F7F
+                                };
+
+    const U64 ROOK_INITIAL_POS = {0x8100000000000081};
+}
 
 
 #endif
