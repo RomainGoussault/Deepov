419909d5c2f7c09746c300324a86b2fc0c23cc45
Navid <navid.hedjazian@gmail.com>
2015-06-17 21:21:52 +0200

    Converts material value to Centipawns

--- src/Eval.hpp
+++ src/Eval.hpp
@@ -13,12 +13,12 @@ public:
 private:
     std::shared_ptr<Board> myBoard;
 
-	const unsigned int KING_VALUE = 100;
-	const unsigned int QUEEN_VALUE = 9;
-	const unsigned int ROOK_VALUE = 5;
-	const unsigned int KNIGHT_VALUE = 3;
-	const unsigned int BISHOP_VALUE = 3;
-	const unsigned int PAWN_VALUE = 1;
+	const unsigned int KING_VALUE = 10000;
+	const unsigned int QUEEN_VALUE = 900;
+	const unsigned int ROOK_VALUE = 500;
+	const unsigned int KNIGHT_VALUE = 310;
+	const unsigned int BISHOP_VALUE = 325;
+	const unsigned int PAWN_VALUE = 100;
 
     int getMobilityScore();
     int getMaterialScore();
