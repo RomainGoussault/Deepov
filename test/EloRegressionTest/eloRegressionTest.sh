77ad581e3da2eb225be853220ff6d2234f59c5f5
Romain Goussault <romain.goussault@gmail.com>
2015-07-05 13:57:58 +0200

    add ELO regression testing using cutechess

---
+++ test/EloRegressionTest/eloRegressionTest.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Starting ELO testing script"
+
+#Copying current version to the test folder
+cp ../../Deepov Deepov
+
+
+#getting latest release
+wget https://github.com/RomainGoussault/Deepov/releases/download/v0.1/Deepov0.1_linux.1
+
+#make it executable
+chmod +x Deepov0.1_linux.1
+
+
+
+./cutechess-cli.sh -engine cmd=Deepov -engine cmd=Deepov0.1_linux.1 -each proto=uci tc=100/20 -rounds 6 -pgnout matchOuput.txt -recover -repeat | while read line; do
+
+	if [[ "$line" == *ELO*  ]]; then
+
+		#Ouput ELO line
+		echo "$line"
+	
+		minusSign=`echo "$line" | grep  -o "-"`
+		#echo "elo diff $minusSign"
+
+		if [[ $minusSign ]]; then
+		
+			echo "Regression"
+			exit 1
+
+		else
+
+			echo "No Regression well done"
+			exit 0
+		fi
+	fi
+done
