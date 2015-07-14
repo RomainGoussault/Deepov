19a6a329f2b3f6fcc80b3d2216efcabb358938b7
NavidHjzn <navid.hedjazian@gmail.com>
2015-07-14 15:04:23 +0200

    Add the Piece.hpp

---
+++ src/Piece.hpp
@@ -0,0 +1,28 @@
+#ifndef PIECES_H
+#define PIECES_H
+
+namespace Piece
+{
+    enum PieceType : int
+    {
+        PAWN_TYPE = 0,
+        KNIGHT_TYPE = 1,
+        BISHOP_TYPE = 2,
+        ROOK_TYPE = 3,
+        QUEEN_TYPE = 4,
+        KING_TYPE = 5,
+        NO_PIECE_TYPE=6
+    };
+
+    enum PieceValue : int
+    {
+        QUEEN_VALUE = 900,
+        ROOK_VALUE = 500,
+        KING_VALUE = 10000,
+        KNIGHT_VALUE = 310,
+        BISHOP_VALUE = 325,
+        PAWN_VALUE = 100
+    };
+};
+
+#endif // PIECES_H
