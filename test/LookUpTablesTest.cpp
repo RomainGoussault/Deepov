8d8a31210b807e2023f36806a5586127c779136f
NavidHjzn <navid.hedjazian@gmail.com>
2015-07-19 23:35:52 +0200

    Add test for LookUpTables

---
+++ test/LookUpTablesTest.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+#include "LookUpTables.hpp"
+
+TEST_CASE( "LookUpTables" )
+{
+    SECTION("Bitboard calculations")
+    {
+        int posIndex = 10;
+        U64 frontMask=LookUpTables::frontBB(posIndex,WHITE);
+        REQUIRE(frontMask == 0x404040404040000);
+
+        frontMask=LookUpTables::frontBB(posIndex,BLACK);
+        REQUIRE(frontMask == 0x4)
+    }
+}
