3831b469ef38783202c0af958e75f92e4516248c
Romain Goussault <romain.goussault@gmail.com>
2015-07-25 00:15:53 +0200

    refactor BitBoardUtils methods

--- src/BitBoardUtils.hpp
+++ src/BitBoardUtils.hpp
@@ -15,20 +15,16 @@ namespace BitBoardUtils
 
 	/* Bit Hacks*/
 	// Source : https://graphics.stanford.edu/~seander/bithacks.html
-	inline unsigned int countBBBitsSet( U64 v)
+	inline unsigned int countBBBitsSet(const U64 bitboard)
 	{
-		v = v - ((v >> 1) & (std::uint64_t)~(std::uint64_t)0/3);                           // temp
-		v = (v & (std::uint64_t)~(std::uint64_t)0/15*3) + ((v >> 2) & (std::uint64_t)~(std::uint64_t)0/15*3);      // temp
-		v = (v + (v >> 4)) & (std::uint64_t)~(std::uint64_t)0/255*15;                      // temp
-		unsigned int c = (std::uint64_t)(v * ((std::uint64_t)~(std::uint64_t)0/255)) >> (sizeof(std::uint64_t) - 1) * CHAR_BIT; // count
-		return c;
+		return __builtin_popcountll(bitboard);
 	}
 
 	//Some relevant links on this function:
 	// https://chessprogramming.wikispaces.com/BitScan
 	// http://stackoverflow.com/questions/671815/
 	// http://stackoverflow.com/questions/20713017/
-	inline int getMsbIndex(const U64 bitboard) {return (63 - __builtin_clzll(bitboard));}
+	inline int getMsbIndex(const U64 bitboard) {return __builtin_ctzll(bitboard);}
 
 	inline bool isBitSet(const U64 bitBoard, const int x, const int y)
 	{
