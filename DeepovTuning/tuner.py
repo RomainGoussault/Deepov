6c583e13f8629e4533a4e52c38bb6796ad441320
NavidHjzn <navid.hedjazian@gmail.com>
2015-09-13 23:51:38 +0200

    Implement cutechess command (32 bit version).

--- DeepovTuning/tuner.py
+++ DeepovTuning/tuner.py
@@ -3,46 +3,56 @@
 
 import os
 import sys
+import subprocess
 import argparse
+import socket
 
 def main(argv):
 	print("Start Deepov tuning v0.1")
 	
-	# Path to the cutechess-cli executable.
-	cutechess_cli_path = './cutechess-cli/cutechess-cli.sh'
-	# The engine whose parameters will be optimized
-	engine = 'cmd=Deepov'
-	# Opponent
-	opponent = 'cmd=Deepov0.2'
-	# Additional cutechess-cli options
-	options = ' -each proto=uci tc=100/1 -rounds 500 -pgnout matchOuput.txt -recover -repeat -openings 			file=openings.pgn'
+	# Default cutechess-cli options
+	protocol = '-each proto=uci '
+	otherOptions = '-recover -repeat -event "Deepov Tuning" -site '
 
 	if argv is None:
 		argv = sys.argv[1:]
 
-	if len(argv) == 0 or argv[0] == '--help':
-		print(__doc__)
-		return 0
-
-	if len(argv) < 3:
-		print('Too few arguments')
-		return 2
-
 	args=createParser()
-	print("List of arguments\n")
-	print(args)
+#	print("List of arguments\n")
+#	print(args)
+	
 
-#	cutechessCommand = cutechess_cli_path + engine + opponent + options
+# Mandatory arguments
+	cutechessCommand = (args.cutechess + ' -engine cmd=' + args.engine + ' dir=' + args.dir +
+	' -engine cmd=' + args.opponent + ' dir=' + args.dir)
+# Optional arguments
+	cutechessCommand += (' -each proto=uci tc=' + args.time + ' -rounds ' + args.rounds + ' -pgnout ' + args.output +
+	' -openings file=' + args.book)
+# Some options set as default
+	cutechessCommand += ' ' + otherOptions + socket.gethostname()
+	
+	if args.verbosity:
+		print('Sending command to cutechess :')
+		print(cutechessCommand)
+		print('\n')
+	
+	runCutechess(cutechessCommand)
+
+	return 0
 
 def createParser():
-	parser = argparse.ArgumentParser(description='Get tuning parameters ...')
-	parser.add_argument("engine", help="Path to the engine to tune.")
-	parser.add_argument("opponent", help="Path to the opponent engine.")
-	parser.add_argument("cutechess", help="Path to cutechess.")
-	parser.add_argument("-v", "--verbose", help="Increase output verbosity.",action="store_true")
-	parser.add_argument("-t", "--time", help="Define the allocated time for engines in cutechess. The format is moves/time+increment, where moves is the number of moves per tc, time is time per tc (either seconds or minutes:seconds), and increment is time increment per move in seconds. Infinite time control can be set with tc=inf (source : cutechess doc).",default="100/1")
-	parser.add_argument("-r", "--rounds", help="Number of rounds to play between the engines",type=int,default=500)
-	parser.add_argument("-b", "--book", help="Openings book file path",default="openings.pgn")
+
+	parser = argparse.ArgumentParser(description='Program to tune Deepov chess engine parameters.')
+	parser.add_argument("engine", help="Executable of the engine to tune.")
+	parser.add_argument("opponent", help="Executable of opponent engine.")
+	parser.add_argument("-d","--dir", help="Path to the engines folder",default='./')
+	parser.add_argument("-c","--cutechess", help="Path to cutechess.",default='./cutechess-cli.sh')
+	parser.add_argument("-v", "--verbosity", help="Increase output verbosity.", action="store_true")
+	parser.add_argument("-t", "--time", help="Define the allocated time for engines in cutechess. The format is moves/time+increment, where moves is the number of moves per tc, time is time per tc (either seconds or minutes:seconds), and increment is time increment per move in seconds. Infinite time control can be set with tc=inf (source : cutechess doc).", default="100/1")
+	parser.add_argument("-r", "--rounds", help="Number of rounds to play between the engines",default='500')
+	parser.add_argument("-b", "--book", help="Openings book file path", default="openings.pgn")
+	parser.add_argument("-o", "--output", help="Output file ", default="a.out")
+
 	return parser.parse_args()
 
 
@@ -54,14 +64,14 @@ def createParser():
 #        usage()
 #        sys.exit(2)          
 
-#def runCutechess(command):
-#	process = subprocess.Popen(cutechessCommand.split())
-#	output = process.communicate()[0]
-#    if process.returncode != 0:
-#        print('Could not execute command: %s' % command)
-#        return 2
+def runCutechess(command):
+	process = subprocess.Popen(command.split())
+	output = process.communicate()[0]
+	if process.returncode != 0:
+		print('Could not execute command: %s' % command)
+		return 2
 
-#	return output
+	return output
 
 
 # execution of the tuning
