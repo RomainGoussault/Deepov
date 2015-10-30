2f54e8a723c5e4beda206971cda5396ee9f4f01b
Romain Goussault <romain.goussault@gmail.com>
2015-10-31 00:07:06 +0100

    Start fixing LINE_BB

--- src/Tables.cpp
+++ src/Tables.cpp
@@ -7,62 +7,74 @@ U64 Tables::PAWN_ATTACK_SPANS[2][64];
 U64 Tables::PASSED_PAWN_MASK[2][64];
 U64 Tables::ATTACK_TABLE[Piece::PIECE_TYPE_NB][64];
 U64 Tables::PAWN_ATTACK_TABLE[Color::COLOR_NB][64];
-U64 Tables::LINE_BB[SQUARE_NB][SQUARE_NB]; // Merge with ATTACK_TABLE ??
+U64 Tables::LINE_BB[SQUARE_NB][SQUARE_NB];
 
 
 /* Methods */
 void Tables::init()
 {
-    for (Square square = SQ_A1; square < SQUARE_NB; ++square)
-    {
-        FRONT_SPANS[WHITE][square] = frontBB(square,WHITE);
-        FRONT_SPANS[BLACK][square] = frontBB(square,BLACK);
-        PAWN_ATTACK_SPANS[WHITE][square] = sidesBB(square,WHITE);
-        PAWN_ATTACK_SPANS[BLACK][square] = sidesBB(square,BLACK);
-        PASSED_PAWN_MASK[WHITE][square] = FRONT_SPANS[WHITE][square] | PAWN_ATTACK_SPANS[WHITE][square];
-        PASSED_PAWN_MASK[BLACK][square] = FRONT_SPANS[BLACK][square] | PAWN_ATTACK_SPANS[BLACK][square];
-        ATTACK_TABLE[Piece::KNIGHT][square] = knightAttacks(square);
-        ATTACK_TABLE[Piece::KING][square] = kingAttacks(square);
-        ATTACK_TABLE[Piece::PAWN][square] = 0x0;
-        ATTACK_TABLE[Piece::BISHOP][square] = bishopAttacks(square);
-        ATTACK_TABLE[Piece::ROOK][square] = rookAttacks(square);
-        ATTACK_TABLE[Piece::QUEEN][square] = queenAttacks(square);
-        ATTACK_TABLE[Piece::NO_PIECE_TYPE][square] = 0x0;
-        PAWN_ATTACK_TABLE[WHITE][square] = pawnAttacks(square,WHITE);
-        PAWN_ATTACK_TABLE[BLACK][square] = pawnAttacks(square,BLACK);
+	for (Square square = SQ_A1; square < SQUARE_NB; ++square)
+	{
+		FRONT_SPANS[WHITE][square] = frontBB(square,WHITE);
+		FRONT_SPANS[BLACK][square] = frontBB(square,BLACK);
+		PAWN_ATTACK_SPANS[WHITE][square] = sidesBB(square,WHITE);
+		PAWN_ATTACK_SPANS[BLACK][square] = sidesBB(square,BLACK);
+		PASSED_PAWN_MASK[WHITE][square] = FRONT_SPANS[WHITE][square] | PAWN_ATTACK_SPANS[WHITE][square];
+		PASSED_PAWN_MASK[BLACK][square] = FRONT_SPANS[BLACK][square] | PAWN_ATTACK_SPANS[BLACK][square];
+		ATTACK_TABLE[Piece::KNIGHT][square] = knightAttacks(square);
+		ATTACK_TABLE[Piece::KING][square] = kingAttacks(square);
+		ATTACK_TABLE[Piece::PAWN][square] = 0x0;
+		ATTACK_TABLE[Piece::BISHOP][square] = bishopAttacks(square);
+		ATTACK_TABLE[Piece::ROOK][square] = rookAttacks(square);
+		ATTACK_TABLE[Piece::QUEEN][square] = queenAttacks(square);
+		ATTACK_TABLE[Piece::NO_PIECE_TYPE][square] = 0x0;
+		PAWN_ATTACK_TABLE[WHITE][square] = pawnAttacks(square,WHITE);
+		PAWN_ATTACK_TABLE[BLACK][square] = pawnAttacks(square,BLACK);
 
-        for (Square square2 = SQ_A1; square2 < SQUARE_NB; ++square2)
-        {
-        	bool AreOnsameLine = getLine(square) == getLine(square);
-        	bool AreOnsameRank = getRank(square) == getRank(square);
+		for (Square square2 = SQ_A1; square2 < SQUARE_NB; ++square2)
+		{
+			bool AreOnsameLine = getLine(square) == getLine(square2);
+			bool AreOnsameRank = getRank(square) == getRank(square2);
 
-        	LINE_BB[square][square2] = AreOnsameLine && AreOnsameRank;
-        }
-    }
+			if(AreOnsameLine)
+			{
+				U64 b = 0LL; //TODO finish implementing: needs to have a bitboard that's a line between square and square2
+				//Note better overload Square and Bitboard operator
+				LINE_BB[square][square2] = b;
+			}
+
+			//ifSameRank
+
+			//ifSameDiag
+
+			//Or use Magic Number?
+
+		}
+	}
 }
 
 U64 Tables::frontBB(const Square pos,Color color)
 {
-    U64 shift = 0xffffffffffffffff << (pos+1); // pos+1 for WHITE, take the complement for BLACk
-    if (color == BLACK){shift = (~shift) >> 1;}
-    return  shift & MASK_FILE[Utils::getFile(pos)];
+	U64 shift = 0xffffffffffffffff << (pos+1); // pos+1 for WHITE, take the complement for BLACk
+	if (color == BLACK){shift = (~shift) >> 1;}
+	return  shift & MASK_FILE[Utils::getFile(pos)];
 }
 
 
 U64 Tables::sidesBB(const Square pos,Color color)
 {
-    unsigned int file = Utils::getFile(pos);
-    U64 leftSide(0);
-    U64 rightSide(0);
-    if (file > 0){leftSide = frontBB(static_cast<Square>(pos-1), color);}
-    if (file < 7){rightSide = frontBB(static_cast<Square>(pos+1), color);}
-    return leftSide | rightSide;
+	unsigned int file = Utils::getFile(pos);
+	U64 leftSide(0);
+	U64 rightSide(0);
+	if (file > 0){leftSide = frontBB(static_cast<Square>(pos-1), color);}
+	if (file < 7){rightSide = frontBB(static_cast<Square>(pos+1), color);}
+	return leftSide | rightSide;
 }
 
 
 U64 Tables::kingAttacks(const Square pos)
 {
-     U64 posBB = 0 | 1LL << pos;
+	U64 posBB = 0 | 1LL << pos;
 	/* Copied from http://pages.cs.wisc.edu/~psilord/blog/data/chess-pages/nonsliding.html */
 	/* we can ignore the rank clipping since the overflow/underflow with
 	respect to rank simply vanishes. We only care about the file
@@ -90,8 +102,8 @@ U64 Tables::kingAttacks(const Square pos)
 U64 Tables::knightAttacks(const Square pos)
 {
 
-    U64 posBB = 0 | 1LL << pos;
-    U64	knight_clip_file_h(posBB & Tables::CLEAR_FILE[7]);
+	U64 posBB = 0 | 1LL << pos;
+	U64	knight_clip_file_h(posBB & Tables::CLEAR_FILE[7]);
 	U64 knight_clip_file_a(posBB & Tables::CLEAR_FILE[0]);
 	U64	knight_clip_file_gh(posBB & Tables::CLEAR_FILE[7] & Tables::CLEAR_FILE[6]);
 	U64 knight_clip_file_ab(posBB & Tables::CLEAR_FILE[0] & Tables::CLEAR_FILE[1]);
@@ -112,20 +124,20 @@ U64 Tables::knightAttacks(const Square pos)
 
 U64 Tables::pawnAttacks(const Square pos, Color color)
 {
-    U64 posBB = 0 | 1LL << pos;
-    U64 leftAttack(0);
-    U64 rightAttack(0);
+	U64 posBB = 0 | 1LL << pos;
+	U64 leftAttack(0);
+	U64 rightAttack(0);
 
-    if (color == WHITE)
-    {
-        leftAttack = (posBB & Tables::CLEAR_FILE[0]) << 7;
-        rightAttack = (posBB & Tables::CLEAR_FILE[7]) << 9;
-    }
-    else if (color == BLACK)
-    {
+	if (color == WHITE)
+	{
+		leftAttack = (posBB & Tables::CLEAR_FILE[0]) << 7;
+		rightAttack = (posBB & Tables::CLEAR_FILE[7]) << 9;
+	}
+	else if (color == BLACK)
+	{
 		leftAttack = (posBB & Tables::CLEAR_FILE[7]) >> 7;
-        rightAttack = (posBB & Tables::CLEAR_FILE[0]) >> 9;
-    }
+		rightAttack = (posBB & Tables::CLEAR_FILE[0]) >> 9;
+	}
 
-    return (leftAttack | rightAttack);
+	return (leftAttack | rightAttack);
 }
