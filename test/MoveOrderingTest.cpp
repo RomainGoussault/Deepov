82c0e2bfd7d10ff20e09313ed9cab76d411c54e4
Romain Goussault <romain.goussault@gmail.com>
2016-05-22 17:17:34 +0200

    Put back MoveOrderingTest
    need to thave test with SEE

--- test/MoveOrderingTest.cpp
+++ test/MoveOrderingTest.cpp
@@ -6,42 +6,42 @@
 #include "Eval.hpp"
 
 
-//TEST_CASE( "Test move rating", "[moveordering]" )
-//{
-//	MagicMoves::initmagicmoves();
-//	Tables::init();
+TEST_CASE( "Test move rating", "[moveordering]" )
+{
+	MagicMoves::initmagicmoves();
+	Tables::init();
 
-//	std::shared_ptr<Board> sp = std::shared_ptr<Board>(new Board("8/8/3k4/4b3/2nnn3/2nQn3/2nnn3/7K w - - 0 1"));
-//	MoveGen moveGen(*sp);
+	std::shared_ptr<Board> sp = std::shared_ptr<Board>(new Board("8/8/3k4/4b3/2nnn3/2nQn3/2nnn3/7K w - - 0 1"));
+	MoveGen moveGen(*sp);
 
-//    std::vector<Move> moveList = moveGen.generateMoves();
-//    
-//    MoveOrdering moveOrder;
-//    moveOrder.rateMoves(moveList,1);
+   std::vector<Move> moveList = moveGen.generateMoves();
+   
+   MoveOrdering moveOrder;
+   moveOrder.rateMoves(moveList,sp, 1, false);
 
-//    for (auto it=moveList.begin(); it<moveList.end(); it++)
-//    {
-//        REQUIRE(it->getMoveRating() == Piece::KNIGHT_VALUE);
-//    }
-//}
+   for (auto it=moveList.begin(); it<moveList.end(); it++)
+   {
+       REQUIRE(it->getMoveRating() == Piece::KNIGHT_VALUE);
+   }
+}
 
 
-//TEST_CASE( "Compare sort methods", "[moveordering]" )
-//{
-//	MagicMoves::initmagicmoves();
-//	Tables::init();
+TEST_CASE( "Compare sort methods", "[moveordering]" )
+{
+	MagicMoves::initmagicmoves();
+	Tables::init();
 
-//	std::shared_ptr<Board> sp = std::shared_ptr<Board>(new Board("r4rk1/ppp1nppp/2n2q2/4p1B1/1b1pP3/2NP1NPb/PPP1QPBP/2KR3R w - - 3 11"));
-//	MoveGen moveGen(*sp);
+	std::shared_ptr<Board> sp = std::shared_ptr<Board>(new Board("r4rk1/ppp1nppp/2n2q2/4p1B1/1b1pP3/2NP1NPb/PPP1QPBP/2KR3R w - - 3 11"));
+	MoveGen moveGen(*sp);
 
-//	std::vector<Move> moveList = moveGen.generateMoves();
-//    
-//    MoveOrdering moveOrder;
-//    moveOrder.rateMoves(moveList,1);
-//    moveOrder.sortMoves(moveList);
+	std::vector<Move> moveList = moveGen.generateMoves();
+   
+   MoveOrdering moveOrder;
+   moveOrder.rateMoves(moveList,sp, 1, false); // TODO test with SEE
+   moveOrder.sortMoves(moveList);
 
-//    Move bestMove=*std::begin(moveList);
+   Move bestMove=*std::begin(moveList);
 
-//    REQUIRE(bestMove.toShortString() == "g5f6");
+   REQUIRE(bestMove.toShortString() == "g5f6");
 
-//}
+}
