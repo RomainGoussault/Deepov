d18eb9f73284b058edc443c37242e4dd7bebff17
Romain Goussault <romain.goussault@gmail.com>
2016-05-24 22:18:15 +0200

    Fix TT test

--- test/TranspositionTableTest.cpp
+++ test/TranspositionTableTest.cpp
@@ -19,10 +19,11 @@ TEST_CASE( "Test TTEntry ", "[TT]")
         REQUIRE(entry1.getNodeType() == NodeType::NONE);
 
         Move nullMove;
-        TTEntry entry2(0,24,-550000,NodeType::EXACT,nullMove);
+        TTEntry entry2(0,24,-550000,NodeType::EXACT,nullMove,3);
         REQUIRE(entry2.getDepth() == 24);
         REQUIRE(entry2.getScore() == -550000);
         REQUIRE(entry2.getNodeType() == 1);
+        REQUIRE(entry2.getTTValue() == TTEntry::K * 24 + 3);
 	}
 
 }
@@ -100,7 +101,7 @@ TEST_CASE( "Search results should be the same with or without TT ", "[TT]")
 		//fens.push_back(""); // Kiwipete
 		fens.push_back("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"); // Kiwipete
 		fens.push_back("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"); //pos 3
-		fens.push_back("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1	"); //pos 4
+		fens.push_back("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - "); //pos 4
 		fens.push_back("rnbqkb1r/pp1p1ppp/2p5/4P3/2B5/8/PPP1NnPP/RNBQK2R w KQkq - 0 6"); //pos 5
 		fens.push_back("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"); //pos 5 bis
 		fens.push_back("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"); // Position 6
@@ -114,9 +115,9 @@ TEST_CASE( "Search results should be the same with or without TT ", "[TT]")
 				REQUIRE(globalTT.calculateEntryCount() == 0);
 				std::shared_ptr<Board> sp = std::shared_ptr<Board>(new Board(fen));
 				Search s(sp);
-				s.negaMaxRoot(i);
-				int nodesSearchWithoutTT = s.myMovesSearched;
 
+				int nodesSearchWithoutTT = s.myMovesSearched;
+				std::cout << i;
 				REQUIRE(globalTT.calculateEntryCount() > 0);
 				Move bestMoveNoTT = s.myBestMove;
 				// search again: we should have the same results (and less nodes searched)
