50fda53f62101f82b46c0ce8d3c489105567ee18
Romain <romain.goussault@gmail.com>
2016-10-16 11:40:33 +0200

    Add header to all files

--- src/MoveGen.cpp
+++ src/MoveGen.cpp
@@ -1,3 +1,22 @@
+/*
+	Deepov, a UCI chess playing engine.
+
+	Copyright (c) 20014-2016 Romain Goussault, Navid Hedjazian
+
+    Deepov is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Deepov is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Deepov.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "MoveGen.hpp"
 #include "BitBoardUtils.hpp"
 #include "MagicMoves.hpp"
